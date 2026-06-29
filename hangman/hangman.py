e03f8e1580e518b948d4de4a386a24c17cb565d6
sana-shaik-17 <sksanathabassum@gmail.com>
2026-06-29 18:42:02 +0530

    Add files via upload

---
+++ hangman/hangman.py
@@ -0,0 +1,99 @@
+import random
+
+# List of words
+words = ["apple", "python", "computer", "school", "flower"]
+
+# Select random word
+word = random.choice(words)
+
+guessed_letters = []
+wrong_guesses = 0
+max_wrong = 6
+
+print("===== HANGMAN GAME =====")
+while wrong_guesses < max_wrong:
+
+    display = ""
+
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+
+    print("\nWord:", display)
+    if "_" not in display:
+      print("\nCongratulations! You guessed the word:", word)
+      break
+
+    guess = input("Enter a letter: ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter only one alphabet.")
+        continue
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in word:
+        print("Correct!")
+    else:
+        wrong_guesses += 1
+        print("Wrong guess!")
+        print("Remaining chances:", max_wrong - wrong_guesses)
+
+if wrong_guesses == max_wrong:
+   import random
+
+# List of words
+words = ["apple", "python", "computer", "school", "flower"]
+
+# Select random word
+word = random.choice(words)
+
+guessed_letters = []
+wrong_guesses = 0
+max_wrong = 6
+
+print("===== HANGMAN GAME =====")
+
+while wrong_guesses < max_wrong:
+
+    display = ""
+
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+
+    print("\nWord:", display)
+
+    if "_" not in display:
+        print("\nCongratulations! You guessed the word:", word)
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter only one alphabet.")
+        continue
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in word:
+        print("Correct!")
+    else:
+        wrong_guesses += 1
+        print("Wrong guess!")
+        print("Remaining chances:", max_wrong - wrong_guesses)
+
+if wrong_guesses == max_wrong:
+    print("\nGame Over!")
+    print("The correct word was:", word) 
